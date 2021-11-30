d8f71e86b114f3b7d1eede9debc400c3744b3fd7
anvudinh <95287279+anvudinh@users.noreply.github.com>
2021-11-30 18:54:38 +0700

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ const CONFIG = {
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Yêu thì yêu mà không yêu thì THÔI <33333333',
     mess: 'chúc mừng bạn đã bị lừa 😘😘',
     messDesc: 'có cl nhé',
     btnAccept: 'Okiiiii lun <3',
